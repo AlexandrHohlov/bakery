fc145079cccfa6e83be7cfe4d8ff2e06fa93caca
AlexanderHohlov <hohlovskiy@gmail.com>
2018-06-25 02:39:32 +0300

    add files

--- app/js/script.custom.js
+++ app/js/script.custom.js
@@ -1,73 +1,35 @@
-// responsiv nav
-
-$('.mobile-btn').click(function(){
-  $(this).toggleClass('active');
-  $('.mobile-menu').toggleClass('active');
-  $('.bg-black').toggleClass('active');
-});
-
-$('.bg-black').click(function(){
-  $('.mobile-btn').removeClass('active');
-  $('.mobile-menu').removeClass('active');
-  $('.bg-black').removeClass('active');    
-});
+$(function () {
 
 
 
+    (function responsiveNav () {
+        let btn = $('#menu-btn');
+        let nav = $('nav');
+        let overlay = $('.overlay');
 
- $(function(){
-        // search
-       $("button").click(function(event){
-            $(".searchBar").animate({
-                width: 'toggle'
-                }, 500, function() {
-                  // animation complete
-            });
+        btn.on('click', function () {
+            nav.addClass('nav--active');
+            overlay.addClass('overlay--active');
+            $('body').addClass('body-fixed');
 
-        });
-        // modal window
-        $('#submitForm').on('submit', function(e){
-            e.preventDefault();
-
-            var n = $(this).find('input[name="name"]').val();
-            var c = $(this).find('textarea[name="email"]').val();
-
-            var data = {
-                name: n,
-                email: c
-            };
-
-            console.info(data);
-
+            $(this).attr('disabled', true);
         });
 
-              $('#search').on('submit', function(e){
-            e.preventDefault();
+        overlay.on('click', function () {
+            nav.removeClass('nav--active');
+            overlay.removeClass('overlay--active');
+            $('body').removeClass('body-fixed');
 
-            var n = $(this).find('input[name="search__text"]').val();
+            btn.removeAttr('disabled');
+        })
+    })();
 
-            var data = {
-                name: n
-            };
+})
 
-            console.info(data);
-
+     // owl-Slider-pancakes
+        $('.owl-carousel').owlCarousel({
+            items:3,
+            nav:true,
+            loop:true,
+            
         });
-         
-    });
-
- // tabs
-
-function openTab(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
